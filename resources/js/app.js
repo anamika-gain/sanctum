19a780321f3b9eee6049c76609ed9508efd1131f
Dipak Sarkar <sdepokbd@gmail.com>
2022-01-04 11:07:37 +0530

    added axios as vue global property

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,26 +1,21 @@
 require('./bootstrap');
+
 import Vue from 'vue';
-import VueRouter from 'vue-router';
 import axios from 'axios';
+import VueRouter from 'vue-router';
 import { routes } from './routes';
 import App from './App.vue';
-import { createApp } from 'vue'
-import axios from 'axios'
-import App from './App.vue'
-const app = createApp(App)
-app.use(number)
-app.config.globalProperties.$axios = axios
-app.mount('#app')
 
+const router = new VueRouter({
+    mode: 'history',
+    routes: routes
+});
 
+// Added axios to vue prototype
+Vue.prototype.$axios = axios
 
- const router = new VueRouter({
-     mode: 'history',
-     routes: routes
- });
-
- const app = new Vue({
-     el: '#app',
-     router: router,
- render: h => h(App),
- });
+const app = new Vue({
+    el: '#app',
+    router: router,
+    render: h => h(App),
+});
